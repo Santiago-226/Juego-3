ee8b06930517540b7a755f7ad8e13f5bc0250bdb
Santiago <santiagosr226@gmail.com>
2025-03-11 18:21:51 -0500

    juego-3

--- juego.js
+++ juego.js
@@ -6,21 +6,33 @@ function generarNumero (){
 puntosGanar = 20;
 
 let ejecutar= true;
-let contador = 0;
+let turno = 1;
 let puntaje1 = 0;
 let puntaje2 = 0
 
 while(ejecutar){
+
     
     puntaje1 = puntaje1 + generarNumero();
-    console.log("puntaje 1 =", puntaje1);
-
     puntaje2 = puntaje2 + generarNumero();
-    console.log("puntaje 2 =", puntaje2);
+
+    console.log(`En el turno ${turno} el jugador 1 acomuló:${puntaje1} y el jugador 2 acomuló:${puntaje2} `);
+    turno++;
+    
 
     if(puntaje1 >= puntosGanar || puntaje2 >= puntosGanar ){
         ejecutar = false;
     }
+}
+
+if(puntaje1 > puntaje2){
+    console.log("El ganador es el jugador 1")
+
+}else if(puntaje2 > puntaje1){
+    console.log("El ganador es el jugador 2")
+
+}else{
+    console.log("Hubo empate")
 }
 
 
